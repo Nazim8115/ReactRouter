d897264abcf638620c35ff7dabac3cfc9731fae9
Nazim8115 <nazim.nn459@gmail.com>
2023-12-17 15:00:55 +0530

    single contact and contact loader

--- routerapp/src/main.jsx
+++ routerapp/src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import ErrorPage from "./error-page";
 import Contact, { loader as contactLoader } from "./routes/contact";
+
 import Root, {
   loader as rootLoader,
   action as rootAction,
@@ -17,6 +18,7 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     loader: rootLoader,
     action: rootAction,
+
     children: [
       {
         path: "contacts/:contactId",
@@ -26,6 +28,7 @@ const router = createBrowserRouter([
     ],
   },
 ]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <>
--- routerapp/src/routes/root.jsx
+++ routerapp/src/routes/root.jsx
@@ -1,9 +1,8 @@
-import { Outlet, Link, useLoaderData, Form } from "react-router-dom";
+import { Link, Outlet, useLoaderData, Form } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
 export async function loader() {
   const contacts = await getContacts();
-  console.log({ contacts });
   return { contacts };
 }
 
@@ -14,7 +13,6 @@ export async function action() {
 
 export default function Root() {
   const { contacts } = useLoaderData();
-  console.log({ contacts });
   return (
     <>
       <div id="sidebar">
@@ -31,9 +29,9 @@ export default function Root() {
             <div id="search-spinner" aria-hidden hidden={true} />
             <div className="sr-only" aria-live="polite"></div>
           </form>
-          <form method="post">
+          <Form method="post">
             <button type="submit">New</button>
-          </form>
+          </Form>
         </div>
         <nav>
           {contacts.length ? (
